2c86ef1acba3a307b0f3dd306db6c573635515c9
Dorsakartalaei <99564710+Dorsakartalaei@users.noreply.github.com>
2022-04-02 13:01:27 -0400

    Add files via upload

---
+++ Sketches/01/Don_1.js
@@ -0,0 +1,48 @@
+
+
+function setup()  {
+createCanvas(800,800)
+background(239, 152, 174)
+
+
+
+}
+
+
+function draw() {
+	noStroke()
+fill(211, 131, 147)
+rect(100, 100, 100, 600)
+
+fill(150, 100, 123)
+noStroke()
+rect(100, 100, 50, 600)
+
+
+fill(160, 60, 85)
+noStroke()
+rect(600, 100, 100, 600)
+
+fill(145, 36, 64)
+noStroke()
+rect(600, 100, 50, 600)
+
+fill(209, 95, 118)
+noStroke()
+triangle(200, 100, 200, 200, 600, 700)
+
+fill(255, 199, 213)
+triangle(200, 200, 200, 300, 600, 700)
+
+fill(255, 180, 200)
+triangle(200, 300, 200, 400, 600, 700)
+
+}
+
+
+
+
+
+
+
+
